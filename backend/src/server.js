4eafe669e7437475af9ab8ad5f36969ed0076ce2
Anurag Kumar Singh <anuragkrsingh3456@gmail.com>
2025-07-11 23:53:05 +0530

    ratelimit

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/ratelimiter.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
@@ -9,7 +10,12 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`Req Method is ${req.method} and Req Url is ${req.url}`);
+  next();
+});
 
+app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
 app.listen(PORT, () => {
